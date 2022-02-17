4931e32fd11596070e0ee5024da3a2f01601ee2b
gabsborges <99661380+gabsborges@users.noreply.github.com>
2022-02-17 18:50:15 -0300

    Add files via upload

---
+++ DDD (1050).cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+	int ddd;
+	printf("Digite seu DDD: ");
+	scanf("%d",&ddd);
+		
+	if (ddd==61){
+		printf("Brasilia");}
+	if (ddd==71){
+		printf("Salvador");}
+	if (ddd==11){
+		printf("Sao paulo");}
+	if (ddd==21){
+		printf("Rio de janeiro");}
+	if (ddd==32){
+		printf("Juiz de fora");}
+	if (ddd==19){
+		printf("Campinas");}
+	if (ddd==27){
+		printf("Vitoria");}
+	if (ddd==31){
+		printf("Belo Horizonte");}
+		
+	else printf("DDD nao cadastrado");
+	
+} 
